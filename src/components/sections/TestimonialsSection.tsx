d2c30d2dc973f287671e803293353e7bce0ec16e
Vhanges <115455282+Vhanges@users.noreply.github.com>
2025-10-31 14:34:12 +0800

    fix(TestimonialsSection): update testimonial images to use customer picture for consistency

--- src/components/sections/TestimonialsSection.tsx
+++ src/components/sections/TestimonialsSection.tsx
@@ -10,21 +10,21 @@ export default function TestimonialsSection(){
             </span> 
 
             <div className="h-auto w-full flex flex-row justify-center gap-10 md:gap-[25px] lg:gap-[50px] overflow-x-auto scrollbar-hide">
-                <TestimonialCard imgSrc="/assets/images/offer-image-2.png">
+                <TestimonialCard imgSrc="/assets/images/customer-picture.png">
                     <p className="w-full text-xl text-white line-clamp-3">
                         We never knew coffee could taste this smooth until we tried <strong>Bobba Union</strong> Every
                         cup feels like a tiny morning ritual — rich, bold, and beautifully balanced.
                         We&apos;ve stopped going to cafes because now my favorite brew is just a kitchen away
                     </p>
                 </TestimonialCard>
-                <TestimonialCard imgSrc="/assets/images/offer-image-2.png">
+                <TestimonialCard imgSrc="/assets/images/customer-picture.png">
                     <p className="w-full text-xl text-white line-clamp-3">
                         Not gonna lie, I wasn’t even a &apos;coffee person&apos;...
                         but now I’m brewing a fresh pot every morning. 
                         Smooth, strong, no bitterness — this changed my whole routine.
                     </p>
                 </TestimonialCard>
-                <TestimonialCard imgSrc="/assets/images/offer-image-2.png">
+                <TestimonialCard imgSrc="/assets/images/customer-picture.png">
                     <p className="w-full text-xl text-white line-clamp-3">
                         Each cup from <strong>Bobba Union</strong> feels like a cozy morning hug.
                         The aroma alone makes me slow down and savor the moment &mdash; it&rsquo;s
