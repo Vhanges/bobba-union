6fd04d1afacc7ad15564a126183197964666f217
Vhanges <115455282+Vhanges@users.noreply.github.com>
2025-11-02 21:53:07 +0800

    fix(JoinUsSection): adjust layout to properly center the section content

--- src/components/sections/JoinUsSection.tsx
+++ src/components/sections/JoinUsSection.tsx
@@ -4,22 +4,25 @@ import CTAButton from '../CTAButton';
 export default function JoinUsSection(){
     return(
         <section className="w-full flex flex-col-reverse lg:flex-row justify-center items-center gap-10 p-[50px]">
-            <div className="flex flex-col">
-                <h1 className="text-4xl md:text-5xl text-primary font-extrabold leading-tight">     
-                    Join the Bobba <br/> Union Family &mdash; <br/> Start Your Franchise <br/> Today!
+            <div className="w-[620px] flex flex-col">
+                <h1 className="text-4xl md:text-5xl text-primary font-extrabold leading-tight">
+                Join the Bobba <br /> Union Family &mdash; <br /> Start Your Franchise <br /> Today!
                 </h1>
-                <CTAButton href="/" className="text-lg md:text-2xl text-center w-auto py-[20px] mt-10">
-                    Let’s Brew Success Together !
+                <CTAButton
+                href="/"
+                className="text-lg md:text-2xl text-center w-auto py-[20px] mt-10"
+                >
+                Let’s Brew Success Together !
                 </CTAButton>
             </div>
             <Image
-                src="/assets/images/bobba-union_building.png"
-                alt="Hero Product"
-                width={751}
-                height={500}
-                quality={85}
-                sizes="(max-width: 640px) 90vw, (max-width: 1024px) 70vw, 751px"
-                className="mx-auto min-w-[300px]"
+            src="/assets/images/bobba-union_building.png"
+            alt="Hero Product"
+            width={550}
+            height={500}
+            quality={85}
+            sizes="(max-width: 640px) 90vw, (max-width: 1024px) 70vw, 500px"
+            className="w-full max-w-[500px] h-auto"
             />
         </section>
     );
