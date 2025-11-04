c51dfb304ecc26d5d68897362613a94277a92c55
Vhanges <115455282+Vhanges@users.noreply.github.com>
2025-11-04 21:07:37 +0800

    fix(JoinUsSection): add padding to the bottom of the section for improved spacing

--- src/components/sections/JoinUsSection.tsx
+++ src/components/sections/JoinUsSection.tsx
@@ -3,7 +3,7 @@ import CTAButton from '../CTAButton';
 
 export default function JoinUsSection(){
     return(
-        <section className="w-full flex flex-col-reverse lg:flex-row justify-center items-center gap-10 p-[50px]">
+        <section className="w-full flex flex-col-reverse lg:flex-row justify-center items-center gap-10 p-[50px] pb-[150px]">
             <div className="w-[620px] flex flex-col">
                 <h1 className="text-4xl md:text-5xl text-primary font-extrabold leading-tight">
                 Join the Bobba <br /> Union Family &mdash; <br /> Start Your Franchise <br /> Today!
@@ -16,13 +16,13 @@ export default function JoinUsSection(){
                 </CTAButton>
             </div>
             <Image
-            src="/assets/images/bobba-union_building.png"
-            alt="Hero Product"
-            width={550}
-            height={500}
-            quality={85}
-            sizes="(max-width: 640px) 90vw, (max-width: 1024px) 70vw, 500px"
-            className="w-full max-w-[500px] h-auto"
+                src="/assets/images/bobba-union_building.png"
+                alt="Hero Product"
+                width={550}
+                height={500}
+                quality={85}
+                sizes="(max-width: 640px) 90vw, (max-width: 1024px) 70vw, 500px"
+                className="w-full max-w-[500px] h-auto"
             />
         </section>
     );
